3001ad68dfe8e022e41dc145e82bdb5a3d083a8c
Dave Miyazaki(PC) <denixden2818@gmail.com>
2025-07-20 21:57:20 -0400

    New Lab18 added

---
+++ Lab18/Exercise18_01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static int assignArrayLength(int arrayLength){
+      
+      return 0;
+
+  }
+
+  public static int getMaxValueInArray(int[] array){
+
+    return 0;
+  }
+
+  public static void assignStudentsScores(int[] studentsScores){
+    return;
+  }
+
+  public static void main(String[] args){
+    Scanner inputRead = new Scanner(System.in);   
+    System.out.print("Enter the number of students: ");
+    final int arrayLength  = inputRead.nextInt();
+
+    int[] studentsScores = new int[arrayLength];
+
+    System.out.print(studentsScores.length);
+
+
+  }
+}
